8437ec7f19e9f51bee9e40723097823b3ef2c5be
LukeJansen <lpjansen@live.co.uk>
2014-07-27 13:38:14 +0100

    First Commit!

---
+++ src/com/LukeJansen/game/Display.java
@@ -0,0 +1,17 @@
+package com.LukeJansen.game;
+
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
+public class Display extends Canvas{
+
+	private static final long serialVersionUID = 1L;
+	int WIDTH = 300;
+	int HEIGHT = 600;
+
+	public static void main(String[] args) {
+		Display game = new Display();
+		JFrame frame = new JFrame();
+	}
+}
